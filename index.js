22c307e24fc586f1b89f4f0e7c945ba46b81c13b
jaqsyadam <157209550+jaqsyadam@users.noreply.github.com>
2025-02-04 13:58:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.use(cors());
 // Подключение маршрутов
 const adminRoutes = require("./routes/admin.route");
 app.use("/api/admin", adminRoutes);
-
-
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/cars", require("./routes/car.route"));
 app.use("/api/bookings", require("./routes/booking.route"));
@@ -24,9 +22,23 @@ app.use("/api/history", require("./routes/history.route"));
 // Подключение статической папки
 app.use(express.static(path.join(__dirname, "public")));
 
-
-// Делаем папку `uploads` доступной для клиента
+// Доступ к папке `uploads`
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Обработка маршрутов для страниц без расширения .html
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+app.get("/cars", (req, res) => res.sendFile(path.join(__dirname, "public", "cars.html")));
+app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "public", "admin.html")));
+app.get("/bookings", (req, res) => res.sendFile(path.join(__dirname, "public", "bookings.html")));
+app.get("/payment", (req, res) => res.sendFile(path.join(__dirname, "public", "payment.html")));
+app.get("/404", (req, res) => res.sendFile(path.join(__dirname, "public", "404.html")));
+app.get("/forgot-password", (req, res) => res.sendFile(path.join(__dirname, "public", "forgot-password.html")));
+app.get("/history", (req, res) => res.sendFile(path.join(__dirname, "public", "history.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "public", "login.html")));
+app.get("/profile", (req, res) => res.sendFile(path.join(__dirname, "public", "profile.html")));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "public", "register.html")));
+app.get("/reset-password", (req, res) => res.sendFile(path.join(__dirname, "public", "reset-password.html")));
+app.get("/verify-otp", (req, res) => res.sendFile(path.join(__dirname, "public", "verify-otp.html")));
 
 
 // Маршрут для любых неизвестных путей
